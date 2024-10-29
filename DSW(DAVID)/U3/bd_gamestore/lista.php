2ad8cb33673cc1c57cbcf8ba655be69d849972f7
kevarm <kevinarmas.77@gmail.com>
2024-10-29 13:12:56 +0000

    29/10

--- DSW(DAVID)/U3/bd_gamestore/lista.php
+++ DSW(DAVID)/U3/bd_gamestore/lista.php
@@ -1,42 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-        require_once __DIR__."/conexion.php";
+    require_once __DIR__ . "/conexion.php";
 
-        $conn= conexion("localhost", "gamestore", "root", "");
+    $conn = conexion("localhost", "gamestore", "postgre", "1234");
 
-        if ($conn == null) {
-            echo "<script>console.log('Error: No se pudo establecer conexión con la base de datos.')</script>";
-          }
-        
-        $resultado_query= $conn->query('SELECT j.nombre AS nombre_juego, 
-        j.genero, 
-        AVG(c.puntos) AS puntuacion, c.comentario
- FROM juegos j 
- LEFT JOIN calificaciones c ON j.id = c.id_juego 
- GROUP BY j.id;
- ORDER BY j.nombre;');
+    if ($conn == null) {
+        echo "<script>console.log('Error: No se pudo establecer conexión con la base de datos.')</script>";
+    }
 
-        while($row = $resultado_query->fetch(PDO::FETCH_OBJ)) {
-            echo "<div>
+    $resultado_query = $conn->query("SELECT j.nombre AS nombre_juego, j.genero, AVG(c.notas) AS puntuacion, 
+    STRING_AGG(c.comentarios, '; '') AS comentarios
+    FROM juegos j 
+    LEFT JOIN calificaciones c ON j.id = c.juego_id
+    GROUP BY j.id, j.nombre, j.genero
+    ORDER BY j.nombre;");
+
+    while ($row = $resultado_query->fetch(PDO::FETCH_OBJ)) {
+        echo "<div>
                       <p>Nombre: {$row->nombre_juego}</p>
                       <p>Género: {$row->genero}</p>
-                      <p>Comentarios: {$row->comentario}</p>
+                      <p>Comentarios: {$row->comentarios}</p>
                       <p>Puntuación media: {$row->puntuacion}</p>
                     </div>
                  ";
-        }
+    }
 
-        $conn=null;
+    $conn = null;
 
 
     ?>
-    
+
 </body>
+
 </html>
