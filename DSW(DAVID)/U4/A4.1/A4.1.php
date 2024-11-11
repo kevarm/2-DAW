315e3bcc1cc00f1cd93cca3a80e0dc902debf9ae
kevarm <kevinarmas.77@gmail.com>
2024-11-11 13:41:07 +0000

    11/11

--- DSW(DAVID)/U4/A4.1/A4.1.php
+++ DSW(DAVID)/U4/A4.1/A4.1.php
@@ -8,10 +8,9 @@
 </head>
 
 <body>
-    <button type="submit">Reiniciar contador</button>
-            
 
-    <?php
+<?php
+
     if (!isset($_COOKIE['visitas'])) {
         setcookie('visitas', '1', time() + 3600 * 24); // Expira en 24 horas
         echo "Bienvenido por primera vez";
@@ -20,8 +19,34 @@
         setcookie('visitas', $visitas, time() + 3600 * 24); // Expira en 24 horas
         echo "Bienvenido por $visitas vez";
     }
+    echo "</br>";
 
+   /* function reiniciar(){
+        $_COOKIE['visitas']=1;*/
+
+    function saludo(){
+        echo "hola";
+    }
+    
+///Crear una cookie
+setcookie("nombre_usuario", "Juan", time() + (86400 * 30), "/"); // Expira en 30 días
+
+// Leer la cookie
+if(isset($_COOKIE["nombre_usuario"])) {
+    echo "Bienvenido de nuevo, " . $_COOKIE["nombre_usuario"];
+} else {
+    echo "Bienvenido, visitante!";
+}
     ?>
+
+
+
+    <br>
+    <input type="submit" onclick= "reiniciar()">Reiniciar contador</input>
+        
+
+
+    
 </body>
 
 </html>
