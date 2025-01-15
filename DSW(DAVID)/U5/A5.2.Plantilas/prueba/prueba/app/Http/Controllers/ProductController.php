31801ea7ab355ad7fa6fe7f0e6a58a7c6ca9f1ae
kevarm <kevinarmas.77@gmail.com>
2025-01-15 12:43:38 +0000

    act a5.4_David

--- DSW(DAVID)/U5/A5.2.Plantilas/prueba/prueba/app/Http/Controllers/ProductController.php
+++ DSW(DAVID)/U5/A5.2.Plantilas/prueba/prueba/app/Http/Controllers/ProductController.php
@@ -5,15 +5,44 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
-{
-    public function index(){
-        $products=[
-            'id','name','description','image','price'
-        ];
+    {
+        public static $products=[
+            [
+                'id'=>1,
+                'name'=>'Product1',
+                'description'=>'description',
+                'image'=>'/img/product1.jpg',
+                'price'=>10
+            ],
+            [
+                'id'=>2,
+                'name'=>'Product2',
+                'description'=>'description',
+                'image'=>'/img/product2.jpg',
+                'price'=>15   
+            ],
+            [
+                'id'=>3,
+                'name'=>'Product3',
+                'description'=>'description',
+                'image'=>'/img/product3.jpg',
+                'price'=>12   
+            ],
+            [
+                'id'=>4,
+                'name'=>'Product4',
+                'description'=>'description',
+                'image'=>'/img/product4.jpg',
+                'price'=>20  
+            ]
+            ];
 
-        $title;
-        $subtitle;
-        $product;
-        return view('product.index');
+        public function index(){
+            $products=[
+                'title'=>'Título',
+                'subtitle'=> 'Nombre',
+                'products'=>'Productos'
+            ];
+            return view('product.index');
+        }
     }
-}
--- DSW(DAVID)/U5/A5.2.Plantilas/prueba/prueba/routes/web.php
+++ DSW(DAVID)/U5/A5.2.Plantilas/prueba/prueba/routes/web.php
@@ -2,14 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 
-/*Route::get('/', function () {
-    $title='Título';
-    return view('/home/index', compact('title'));
-})->name('home');*/
 
 Route::get('/about',[HomeController::class,'about'])->name('about');
 
 Route::get('/', [HomeController::class,'index'])->name('home');
 
 Route::get('/product', [ProductController::class,'index'])->name('product');
+
